fb2e070dc26ee5b62de97c0176341cad811d0694
Brayan <brayanhdz1930@gmail.com>
2025-08-28 09:35:12 -0600

    lab4

--- Laboratorio 4.R
+++ Laboratorio 4.R
@@ -5,7 +5,7 @@
 
 # Importar datos de indice de calidad
 
-calidad <- read.csv("calidad_planta.csv" header = T)
+calidad <- read.csv("CalidadPlantas1.csv", header= T)
 
 calidad$Tratamiento <- as.factor(calidad$Tratamiento)
 
@@ -23,6 +23,8 @@ boxplot(calidad$IE - calidad$Tratamiento,
 
 tapply(calidad$IE, calidad$Tratamiento, mean)
 tapply(calidad$IE, calidad$Tratamiento,  var)
+tapply(calidad$IE, calidad$Tratamiento,  sd)
+
 
 # Observamos que la varianza del grupo  fert es 3 veces 
 # mas grande que el grupo Control (ctrl)
@@ -33,5 +35,32 @@ library("ggplot2")
 ggplot(calidad, aes(x = IE, color = Tratamiento))+ geom_density()
 ggplot(calidad, aes(x = IE, color = Tratamiento))+ geom_histogram()
 
+# Separar los datos por tratamiento
 
+df_ctrl <- subset(calidad, Tratamiento == "Ctrl")
+df_ctrl <- subset(calidad, Tratamiento != "Ctrl")
 
+#qqnorm revisar normalidad
+par(afrow = c(2,2))
+qqnorm(df_ctlr$IE); qqline(df_ctlr$IE)
+qqnorm(df_fert$IE); qqline(df_fert$IE)
+par(afrow = c(1,1))
+
+# revisar homogenidad de varianzas
+var.test(df_ctlr$IE,df_fest$IE)
+var.test(calidad$IE - calidad$Tratamiento)
+
+# Aplicar la prueba de t, variables iguales 
+# dos colores =two.sided
+
+t.test(calidad$IE, calidad$Tratamiento,
+       var.equal = T,
+       alternative = "two.sided")
+
+cohens_efecto <- function(x, y){
+  n1 <- length(x); n2 <- sd(y)
+  s1 <- sd(x); s2 <- sd(y)
+  sp <- sqrt((n1 - 1) * s1´2 + (n1 - 1) * s´2) / (n1 + n2 -2)) 
+  (mean(x) - mean(y)) / sp}
+
+d_cal_efect
